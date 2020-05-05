dda6fa833edcbe17fd96015d4603746515513afa
Stephen SOLOMON <stephensololmon48@gmail.com>
2020-05-05 16:42:21 +0100

    added remote credentials

--- database/conn.php
+++ database/conn.php
@@ -1,17 +1,17 @@
 <?php
 //the local  machine  environment database connections
-$host = '127.0.0.1';
-$db = 'gmlp';
-$username = 'root';
-$pass = '';
-$charset = 'utf8mb4';
+// $host = '127.0.0.1';
+// $db = 'gmlp';
+// $username = 'root';
+// $pass = '';
+// $charset = 'utf8mb4';
 
 //the remote server environment database connections
-/*$host = 'us-cdbr-iron-east-01.cleardb.net';
-$db = 'heroku_ef9afaef23699ff';
-$username = 'bc337d31c2f817';
-$pass = ' 2947a474';
-$charset = 'utf8mb4';*/
+$host = 'ol5tz0yvwp930510.cbetxkdyhwsb.us-east-1.rds.amazonaws.com';
+$db = 'tyegkr1szm5vwd8a';
+$username = 'l104kv1o1q6abaz1';
+$pass = 'f938oeksqmcobz70';
+$charset = 'utf8mb4';
 
 $dsn = "mysql:host=$host;dbname=$db;charset=$charset";
 try {
--- thirdpartyaccess/config/Database.php
+++ thirdpartyaccess/config/Database.php
@@ -1,10 +1,15 @@
 <?php
 
     class Database {
-        private $host = 'localhost';
-        private $db_name = 'gmlp';
-        private $username = 'root';
-        private $password = '';
+        // private $host = 'localhost';
+        // private $db_name = 'gmlp';
+        // private $username = 'root';
+        // private $password = '';
+
+        private $host = 'ol5tz0yvwp930510.cbetxkdyhwsb.us-east-1.rds.amazonaws.com';
+        private $db_name = 'tyegkr1szm5vwd8a';
+        private $username = 'l104kv1o1q6abaz1';
+        private $password = 'f938oeksqmcobz70';
 
         private $conn;
 
