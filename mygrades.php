b22afb27161dda1bf1a43de08bda1cb6aa82a332
Alamin-Juma <alaminjuma96@gmail.com>
2020-05-10 05:55:19 +0300

    mygrade.php  display score data

--- mygrades.php
+++ mygrades.php
@@ -5,18 +5,20 @@ require_once 'includes/header.php';
 require_once 'database/conn.php';
 require_once 'includes/auth_check.php';
 
+
 ?>
 
 <div class="card" style="width: 18rem;">
         <div class="card-body">
             <h5 class="card-title"><?php echo $result['firstname'] . "\t" . $result['secondname']; ?></h5>
-            <h6 class="card-subtitle mb-2 text-muted"><?php echo $result['role']; ?></h6>
-            <p class="card-text">Email:<?php echo $result['email']; ?></p>
-            <p class="card-text">School ID:<?php echo $result['schoolname']; ?></p>
-            <p class="card-text">School Name:<?php echo $result['game_name'] . "\t" . $result['play_mode']; ?></p>
-            <p class="card-text">School Address:<?php echo $result['score']; ?></p>
-            <p class="card-text">School Name:<?php echo $result['start_time']; ?></p>
-            <p class="card-text">School Address:<?php echo $result['end_time']; ?></p>
+            <h6 class="card-subtitle mb-2 text-muted">Email:<?php echo $result['email']; ?></h6>
+            <p class="card-text">School Name:<?php echo $result['schoolname']; ?></p>
+            <p class="card-text">Play Mode:<?php echo $result['play_mode']; ?></p>
+
+            <p class="card-text">Game Name:<?php echo $result['game_name']; ?></p>
+            <p class="card-text">Score:<?php echo $result['score']; ?></p>
+            <p class="card-text">Start Time:<?php echo $result['start_time']; ?></p>
+            <p class="card-text">End Time:<?php echo $result['end_time']; ?></p>
             <p>
                 <a href="schoolManagerSchoolAdminViewUsers.php?id=<?php echo $result['user_id']; ?>"
                    class="btn btn-primary"> Users</a>
