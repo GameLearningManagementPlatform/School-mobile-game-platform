b0f48a6ffea8307d8befec6c3d512b3bda897fba
Alamin-Juma <alaminjuma96@gmail.com>
2020-05-12 03:23:53 +0300

    student curve activity

--- LineGraph.php
+++ LineGraph.php
@@ -1,11 +1,20 @@
+<?php
+$title = 'Student grades';
 
+require_once 'includes/header.php';
+require_once 'database/conn.php';
+require_once 'includes/auth_check.php';
+
+$result = $Student_Crud->getCurrentStudentScore($_SESSION['email']);
+
+?>
     <script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
     <script type="text/javascript">
         google.charts.load('current', {'packages':['corechart']});
         google.charts.setOnLoadCallback(drawChart);
 
         function drawChart() {
-            var data = google.visualization.arrayToDataTable([
+            var data = google.visualization.DataTable([
                 ['end_time', 'scores'],
                 <?php
                 $date = $result['end_time'];
@@ -25,7 +34,6 @@
             chart.draw(data, options);
         }
     </script>
-</head>
 <body>
 <div id="curve_chart" style="width: 900px; height: 500px"></div>
 </body>
