b76dfcc5a762c5531d36fc3e80e91421bea87914
Alamin-Juma <alaminjuma96@gmail.com>
2020-05-12 05:00:43 +0300

    student curve activity

--- test.php
+++ test.php
@@ -1,5 +1,6 @@
 <?php
 
+
 $result = $Student_Crud->getAllScores();
 ?>
 
@@ -7,13 +8,13 @@ $result = $Student_Crud->getAllScores();
     <table class="table container">
         <thead class="thead-dark">
         <tr>
-            <th> email</th>
+            <th> player_id</th>
             <th> score</th>
         </tr>
         </thead>
         <?php while ($row = $result->fetch(PDO::FETCH_ASSOC)): ?>
             <tr>
-                <td> <?php echo $_SESSION['email']; ?></td>
+                <td> <?php echo $_SESSION['player_id']; ?></td>
 
                 <td> <?php echo $row['score']; ?></td>
             </tr>
